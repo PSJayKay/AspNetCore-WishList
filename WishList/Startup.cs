7bf655ec411931b5c8913608d8dc7f629c8862fb
PSJayKay <32443244+PSJayKay@users.noreply.github.com>
2019-05-15 16:07:37 -0500

    Completed the module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -16,11 +16,14 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvcWithDefaultRoute();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,7 +33,8 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseMvcWithDefaultRoute();
+
+            
             
         }
     }
