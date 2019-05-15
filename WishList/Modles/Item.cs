8a04bc84dcc8f797aa14f245b3cfbe3536bd1488
PSJayKay <32443244+PSJayKay@users.noreply.github.com>
2019-05-15 16:28:54 -0500

    Completed the module

---
+++ WishList/Modles/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Modles
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
